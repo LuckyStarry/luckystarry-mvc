9a9130fa97ca3b03285f982990c804864add80d0
SUN BO <starry@vip.qq.com>
2018-09-04 18:11:27 +0800

    移除 webpack 打包

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,20 @@
-//gulpfile.js
-let gulp = require('gulp');
-let ts = require('gulp-typescript');
-let tsp = ts.createProject('tsconfig.json');
-
-//目录常量
+const gulp = require('gulp')
+const del = require('del')
+const ts = require('gulp-typescript')
+const tsp = ts.createProject('tsconfig.json')
 const PATHS = {
   scripts: ['./src/**/*.ts'],
   output: './dist'
-};
-// 编译ts文件
-gulp.task('build-ts', function() {
+}
+
+gulp.task('clean-dist', function() {
+  return del([PATHS.output])
+})
+
+gulp.task('build-ts', ['clean-dist'], function() {
   return gulp.src(PATHS.scripts)
     .pipe(tsp())
-    .pipe(gulp.dest(PATHS.output));
-});
-// 正式编译
+    .pipe(gulp.dest(PATHS.output))
+})
+
 gulp.task('build', ['build-ts']);
