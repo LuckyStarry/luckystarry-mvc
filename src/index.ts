c3239d1f9ea255d263d9a7fcebec90720f43296e
SUN BO <starry@vip.qq.com>
2018-09-20 17:38:21 +0800

    luckystarry-ioc => 0.1.0

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,2 @@
-import { IServiceContainer, ServiceContainer } from './service-container'
-import { Injectable } from './injectable'
 import * as web from './web'
-export { IServiceContainer, ServiceContainer, Injectable, web }
-export default { ServiceContainer, Injectable, web }
+export { web }
