0f67c5537915a53f1738d4389eb6043006ea0109
SUN BO <starry@vip.qq.com>
2020-02-26 08:43:27 +0800

    增加测试用例

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -38,4 +38,40 @@ describe('Index', function() {
     expect(mvc.FromRoute).not.undefined
     expect(typeof mvc.FromRoute).to.equal('function')
   })
+
+  it('存在 mvc.ApplicationBuider', function() {
+    expect(mvc.ApplicationBuider).not.null
+    expect(mvc.ApplicationBuider).not.undefined
+    expect(typeof mvc.ApplicationBuider).to.equal('function')
+  })
+
+  it('存在 mvc.Mapping', function() {
+    expect(mvc.Mapping).not.null
+    expect(mvc.Mapping).not.undefined
+    expect(typeof mvc.Mapping).to.equal('function')
+  })
+
+  it('存在 mvc.MappingDelete', function() {
+    expect(mvc.MappingDelete).not.null
+    expect(mvc.MappingDelete).not.undefined
+    expect(typeof mvc.MappingDelete).to.equal('function')
+  })
+
+  it('存在 mvc.MappingGet', function() {
+    expect(mvc.MappingGet).not.null
+    expect(mvc.MappingGet).not.undefined
+    expect(typeof mvc.MappingGet).to.equal('function')
+  })
+
+  it('存在 mvc.MappingPost', function() {
+    expect(mvc.MappingPost).not.null
+    expect(mvc.MappingPost).not.undefined
+    expect(typeof mvc.MappingPost).to.equal('function')
+  })
+
+  it('存在 mvc.MappingPut', function() {
+    expect(mvc.MappingPut).not.null
+    expect(mvc.MappingPut).not.undefined
+    expect(typeof mvc.MappingPut).to.equal('function')
+  })
 })
